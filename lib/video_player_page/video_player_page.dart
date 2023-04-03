da5b377b8d590133b671a836d16e8e44601ec068
izi4ever <mynotedata@gmail.com>
2023-04-03 22:23:17 +0200

    Finished video player widget

--- lib/video_player_page/video_player_page.dart
+++ lib/video_player_page/video_player_page.dart
@@ -6,6 +6,9 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
 
+import '../custom/custom_constants.dart';
+import '../home_page/dictionary_widget/dictionary_widget.dart';
+
 class VideoPlayerPage extends StatefulWidget {
   const VideoPlayerPage({
     super.key,
@@ -22,6 +25,8 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   late VideoPlayerController _videoPlayerController;
   ChewieController? _chewieController;
   int? bufferDelay;
+
+  GlobalKey ratioContainer = GlobalKey();
 
   @override
   void initState() {
@@ -56,14 +61,9 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
       progressIndicatorDelay: bufferDelay != null ? Duration(milliseconds: bufferDelay!) : null,
       hideControlsTimer: const Duration(seconds: 5),
       allowFullScreen: false,
-      placeholder: IconButton(
-        icon: const Icon(
-          Icons.arrow_back,
-          color: Colors.white,
-        ),
-        onPressed: Get.back,
-      ),
-      // aspectRatio: ,
+      // aspectRatio: _videoPlayerController.value.aspectRatio,
+      // aspectRatio: 2.35,
+      // aspectRatio: ratioContainer.currentContext!.size!.width / ratioContainer.currentContext!.size!.height,
       zoomAndPan: true,
     );
   }
@@ -72,21 +72,61 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(title: const Text('Video Player Widget')),
-      body: Container(
-        color: Colors.black,
-        child: Center(
-          child: _chewieController != null && _chewieController!.videoPlayerController.value.isInitialized
-              ? Chewie(
-                  controller: _chewieController!,
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircularProgressIndicator(),
-                  ],
-                ),
-        ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: DictionaryWidget(),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            // If keyboard is/not active
+            bottom: (MediaQuery.of(context).viewInsets.bottom == 0)
+                ? (MediaQuery.of(context).orientation == Orientation.portrait)
+                    ? CustomConstants.lessonHeightPortrait
+                    : CustomConstants.lessonHeightLandscape
+                : 0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                // If keyboard is/not active
+                bottomLeft: (MediaQuery.of(context).viewInsets.bottom == 0)
+                    ? const Radius.circular(CustomConstants.webviewRadius)
+                    : const Radius.circular(0),
+                bottomRight: (MediaQuery.of(context).viewInsets.bottom == 0)
+                    ? const Radius.circular(CustomConstants.webviewRadius)
+                    : const Radius.circular(0),
+              ),
+              child: Container(
+                key: ratioContainer,
+                color: Colors.black,
+                child: _chewieController != null && _chewieController!.videoPlayerController.value.isInitialized
+                    ? Chewie(
+                        controller: _chewieController!,
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          CircularProgressIndicator(),
+                        ],
+                      ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: IconButton(
+              onPressed: Get.back,
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+            ),
+          ),
+        ],
       ),
+      backgroundColor: Colors.black,
     );
   }
 }
